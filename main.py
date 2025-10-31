5fe8e5fb0293601d366faca8e59a8162f236608f
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-10-31 15:56:35 +0000

    renamed variables

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Parameters
-u = 5
-a = 2
-t = np.linspace(0, 10, 100)
+# Parameters (using Numpy -> look it up!)
+velocity_initial = 5
+acceleration = 2
+time = np.linspace(0, 10, 100)
 
-# Kinematic Equations
-s = (u * t) + (0.5 * a * t**2)
-v = u + (a * t)
-a_values = a * np.ones_like(t)
-s_final = s[-1]
-print(f"Displacement at t=10 seconds: {s_final:.2f} meters")
+# Kinematic Equations (using Numpy -> look it up!)
+position_eqn = (velocity_initial * time) + (0.5 * acceleration * (time**2))
+velocity_eqn = velocity_initial + (acceleration * time)
+a_values = acceleration * np.ones_like(time)
+position_final = position_eqn[-1]
+print(f"Displacement at t=10 seconds: {position_final:.2f} meters")
 
-# Plotting Data
+# Plotting Data with Matplotlib functions
 plt.figure(figsize=(4, 4))
 
 plt.subplot(3, 1, 1)
-plt.plot(t, s, '--c')
+plt.plot(time, position_eqn, '--c')
 plt.title('Position vs Time', fontsize=10)
 plt.ylabel('Position (m)', fontsize=8)
 plt.grid(True)
 
 plt.subplot(3, 1, 2)
-plt.plot(t, v, ':r')
+plt.plot(time, velocity_eqn, ':r')
 plt.title('Velocity vs Time', fontsize=10)
 plt.ylabel('Velocity (m/s)', fontsize=8)
 plt.grid(True)
 
 plt.subplot(3, 1, 3)
-plt.plot(t, a_values, '-.m', linewidth=2)
+plt.plot(time, a_values, '-.m', linewidth=2)
 plt.title('Acceleration vs Time', fontsize=10)
 plt.xlabel('Time (s)', fontsize=8)
 plt.ylabel('Acceleration (m/s$^2$)', fontsize=8)
